9ebc857a3651bf71e5b4c5beea590cbd695172cc
rafaelfs96 <rafaelfs96@outlook.com>
2021-02-17 18:23:27 -0300

    create product delete methods

--- frontend/src/app/components/product/product-delete/product-delete.component.ts
+++ frontend/src/app/components/product/product-delete/product-delete.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { ActivatedRoute, Router } from '@angular/router'
+import { Product } from '../Product.model'
+import { ProductService } from '../product.service'
 
 @Component({
   selector: 'app-product-delete',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-delete.component.scss']
 })
 export class ProductDeleteComponent implements OnInit {
+  product: Product
 
-  constructor() { }
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    })
   }
 
+  deleteProduct(): void {
+    this.productService.delete(this.product.id).subscribe(() => {
+      this.productService.showMessage('product deleted')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 }

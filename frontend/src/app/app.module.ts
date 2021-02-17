003ce7a8ef66bb981255fe99b12ab72ade533a34
rafaelfs96 <rafaelfs96@outlook.com>
2021-02-17 17:31:43 -0300

    add locale settings to app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser'
-import { NgModule } from '@angular/core'
+import { NgModule, LOCALE_ID } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -24,17 +24,28 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input';
-import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input'
+import { ProductReadComponent } from './components/product/product-read/product-read.component'
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component'
+import { MatTableModule } from '@angular/material/table'
+import { MatPaginatorModule } from '@angular/material/paginator'
 import { MatSortModule } from '@angular/material/sort'
 
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component'
+
+registerLocaleData(localePt)
+
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, ProductCrudComponent, ProductCreateComponent, ProductReadComponent, ProductRead2Component],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, ProductCrudComponent, ProductCreateComponent, ProductReadComponent, ProductRead2Component, ProductUpdateComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatSidenavModule, MatListModule, MatCardModule, MatButtonModule, MatSnackBarModule, HttpClientModule, MatInputModule, MatFormFieldModule, FormsModule, MatTableModule, MatPaginatorModule, MatSortModule],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-br'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
